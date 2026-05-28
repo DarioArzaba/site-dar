58d6ba009b4084818ff594661fe0799db61dcafe
Dario Arzaba <dario.arzaba@gmail.com>
2026-05-28 13:06:14 -0600

    remove adapter from wrangler

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,15 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
 import mdx from '@astrojs/mdx';
 import remarkMath from "remark-math"
 import rehypeKatex from 'rehype-katex';
 
-import cloudflare from '@astrojs/cloudflare';
-
-// https://astro.build/config
 export default defineConfig({
-  site: "https:darioarzaba.com",
+  site: "https://darioarzaba.com",
+  output: "static",
   integrations: [react(), mdx()],
-
   markdown: {
     remarkPlugins: [remarkMath],
     rehypePlugins: [rehypeKatex],
-  },
-
-  adapter: cloudflare()
+  }
 });
